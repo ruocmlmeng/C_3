16126021d8c3704a9709ce3de59f02db122ec65d
wangjun <1556405648@qq.com>
2024-07-03 21:34:06 +0800

    作业

--- test_7_2.c
+++ test_7_2.c
@@ -45,35 +45,96 @@
 //     printf("奖金为%d\n",money);
 //     return 0;
 // }
+// int main()
+// {
+//   double num1 = 0;
+//   double num2 = 0;
+//   char ch = 0;
+//   double res = 0;
+//   printf("请输入你要进行的操作: (例如: 1+2) \n");
+//   scanf("%lf%c%lf",&num1,&ch,&num2);
+//   switch(ch)
+//   {
+//     case '+':
+//       res = num1 + num2;
+//     break;
+//     case '-':
+//       res = num1 - num2;
+//     break;
+//     case '*':
+//       res = num1 * num2;
+//     break;
+//     case '/':
+//       res =  num1 / num2;
+//     break;
+//     case '%':
+//       res = (int )num1 % (int )num2;
+//     break;
+//     default:
+//       printf("输入的运算符错误!请重新输入.\n");
+//     break;
+//   }
+//   printf("%.1lf %c %.1lf = %.1lf\n",num1,ch,num2,res);
+//   return 0;
+// }
+// int main()
+// {
+//   int j = 0;
+//   int i = 10;
+//   if( j = 0)
+//     i++;
+//   else
+//     i--;
+//   printf("%d\n",i);
+//
+//   return 0;
+// }
+// int main()
+// {
+//     int i = 0;
+//     int flag = 0;//记录每个位的三次幂
+//     int sum = 0;//记录每个位的三次幂的和
+//     for(i = 100; i <1000;i++)
+//     {
+//         sum = 0;
+//         int res = i;//备份i
+//         //求出个位，十位，百位
+//         while(res)
+//         {
+//             flag = res % 10;
+//             sum = sum + flag * flag *flag;
+//             res = res / 10;
+//             flag = 0;
+//         }
+//         if(sum == i)
+//         {
+//             printf("%d ",i);
+//         }
+//
+//     }
+//     printf("\n");
+//     return 0;
+//
+// }
 int main()
 {
-  double num1 = 0;
-  double num2 = 0;
-  char ch = 0;
-  double res = 0;
-  printf("请输入你要进行的操作: (例如: 1+2) \n");
-  scanf("%lf%c%lf",&num1,&ch,&num2);
-  switch(ch)
+  int i = 0;
+  int sum = 0;//记录正整数
+  int num = 0;
+  scanf("%d",&num);
+  for(i = 1; i < num;i++)
   {
-    case '+':
-      res = num1 + num2;
-    break;
-    case '-':
-      res = num1 - num2;
-    break;
-    case '*':
-      res = num1 * num2;
-    break;
-    case '/':
-      res =  num1 / num2;
-    break;
-    case '%':
-      res = (int )num1 % (int )num2;
-    break;
-    default:
-      printf("输入的运算符错误!请重新输入.\n");
-    break;
+    int j = 0;
+    for(j = i; j < num ;i++)
+    {
+      sum = sum + j;
+      if(sum == num)
+      {
+        printf("%d\n", j);
+        break;
+      }
+    }
+    sum = 0;
   }
-  printf("%.1lf %c %.1lf = %.1lf\n",num1,ch,num2,res);
   return 0;
 }
