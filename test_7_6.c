ef59e3244b90e6e607dd1b1d4fe1057c8f8eb0f1
wangjun <1556405648@qq.com>
2024-07-06 10:01:38 +0800

    复习

--- test_7_6.c
+++ test_7_6.c
@@ -99,45 +99,100 @@
  *二分查找
  *
  */
+// #define N 10
+// int main()
+// {
+//     int a[N];
+//     printf("请输入十个数：\n");
+//     int i = 0;
+//     for(i = 0 ; i < N ;i++)
+//     {
+//         scanf("%d",&a[i]);
+//     }
+//     printf("请输入x:\n");
+//     int x = 0;
+//     scanf("%d",&x);
+//     int l = 0;
+//     int r = N - 1;
+//     while(l <= r)
+//     {
+//         int mid = (r + r) / 2;
+//         if(a[mid] > x)
+//         {
+//             r = mid - 1;
+//         }
+//         else if(a[mid] < x)
+//         {
+//             l = mid + 1;
+//         }
+//         else
+//         {
+//             break;
+//         }
+//     }
+//     if(l <= r)
+//     {
+//         printf("YES\n");
+//     }
+//     else
+//     {
+//         printf("NO\n");
+//     }
+//
+//     return 0;
+// }
+/*
+ *
+ *插入排序-以二分查找的形式
+ *
+ */
+/*
+ * 定义一个Insert函数
+ * @a：数组名
+ * @n：数组元素的个数
+ * @x：要二分查找插入的数
+ */
+void Insert(int a[],int n,int x)
+{
+  int l = 0;
+  int r = n - 2;
+  while (l <=r)
+  {
+   int mid = (l + r ) / 2;
+   if(a[mid] > x)
+   {
+    r = mid - 1;
+   }
+   else if(a[mid] < x)
+   {
+    l = mid + 1;
+   }
+  }
+  //找到了之后就进行插入操作
+  int k = 0;
+  for(k = n - 2 ; k >=l;k--)
+  {
+   a[k+1] = a[k];
+  }
+  a[l] = x;
+}
 #define N 10
 int main()
 {
-    int a[N];
-    printf("请输入十个数：\n");
-    int i = 0;
-    for(i = 0 ; i < N ;i++)
-    {
-        scanf("%d",&a[i]);
-    }
-    printf("请输入x:\n");
-    int x = 0;
-    scanf("%d",&x);
-    int l = 0;
-    int r = N - 1;
-    while(l <= r)
-    {
-        int mid = (r + r) / 2;
-        if(a[mid] > x)
-        {
-            r = mid - 1;
-        }
-        else if(a[mid] < x)
-        {
-            l = mid + 1;
-        }
-        else
-        {
-            break;
-        }
-    }
-    if(l <= r)
-    {
-        printf("YES\n");
-    }
-    else
-    {
-        printf("NO\n");
-    }
-
-    return 0;
+ int a[N];
+ printf("请输入十个数：\n");
+ int i = 0;
+ for(i = 0 ; i < N ;i++)
+ {
+  scanf("%d",&a[i]);
+ }
+ for (i = 1; i < N ;i++)
+ {
+   Insert(a,i+1,a[i]);
+ }
+ for(i = 0 ; i < N ;i++)
+ {
+  printf("%d ",a[i]);
+ }
+ return 0;
 }
