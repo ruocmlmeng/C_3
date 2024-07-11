fd10b9797b3c5e655f3bc3153373ac1ae3796661
wangjun <1556405648@qq.com>
2024-07-11 13:14:46 +0800

    最大公因数

--- test_7_11.c
+++ test_7_11.c
@@ -7,3 +7,56 @@
   * @date           : 2024/7/11
   ******************************************************************************
   */
+#include<stdio.h>
+/*
+ *
+ * 求最大公约数:欧几里得算法
+ */
+/*
+ *  Gcd：求两个数的最大公约数
+ *  @x,y:要求的两个数
+ *  返回值：
+ *      两个数的最大公约数
+ */
+// int Gcd(int x,int y)
+// {
+//     int z = 0;
+//     while (y)
+//     {
+//         z =  x  % y;
+//         x = y;
+//         y = z;
+//     }
+//     return x;
+// }
+// int main()
+// {
+//     int x = 0;
+//     int y = 0;
+//     scanf("%d%d",&x,&y);
+//     int res = Gcd(x,y);
+//     printf("%d与%d的最大公约数是%d\n",x,y,res);
+//     return 0;
+// }
+/*
+ *
+ * 求最大公因数-定义法
+ */
+// #define min(a,b) (a < b) ? ( a) : (b)
+// int main()
+// {
+//  int x = 0;
+//  int y = 0;
+//  scanf("%d%d",&x,&y);
+//  int res = min(x,y);
+//  int  i = 0;
+//  for (i = res ; i >= 1 ;i--)
+//  {
+//   if(x % i == 0 && y % i == 0)
+//   {
+//     printf("最大公约数是%d\n",i);
+//     break;
+//   }
+//  }
+//  return 0;
+// }
