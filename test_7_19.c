bb8f8112ce3b48ee21f9b4adfe439992b5896e74
wangjun <1556405648@qq.com>
2024-07-19 20:46:16 +0800

    最大公因数

--- test_7_19.c
+++ test_7_19.c
@@ -60,6 +60,26 @@
 //     printf("最大公因数%d\n",gcd);
 //     return 0;
 // }
+#define min(a,b) ( (a) < (b) ? (a) : (b))
+int main()
+{
+    int a = 0;
+    int b = 0;
+    scanf("%d %d",&a,&b);
+    int x = min(a,b);
+    int i = 0;
+
+    for(i = min(a,b); i>=1 ;i--)
+    {
+        if(a % i == 0 && b % i == 0)
+        {
+            printf("最大公因数%d\n",i);
+            break;
+        }
+    }
+
+    return 0;
+}
 // int main()
 // {
 //     int a = 0;
