b442f1268f5d51400697ef718a100354c12dfa88
wangjun <1556405648@qq.com>
2024-07-20 16:30:59 +0800

    插入排序-二分法

--- test_7_20.c
+++ test_7_20.c
@@ -52,30 +52,82 @@
  *
  * 插入排序-遍历的方式排序
  */
+// int main()
+// {
+//     int a[10] = {2,3,1,5,9,6,4,8,0,10};
+//     int i = 0;
+//     int j = 0;
+//     int n = sizeof(a) /sizeof(a[0]);
+//     int k = 0;
+//     for(i = 1 ; i < n-1;i++)
+//     {
+//         for(j = 0 ; j < i ;j++)
+//         {
+//             if(a[j] > a[i])
+//             {
+//                 break;
+//             }
+//         }
+//         int x = a[i];
+//         for (k = i - 1; k >= j; k--)
+//         {
+//             a[k+1] = a[k];
+//         }
+//         a[j] = x;
+//     }
+//     for(i = 0 ; i < n ;i++)
+//     {
+//         printf("%d ",a[i]);
+//     }
+//     return 0;
+// }
+
+/*
+ * 插入排序-二分插入排序
+ *
+ */
+/*
+ *    Insert: 二分插入排序
+ *    @a：数组名
+ *    @n：数组的大小
+ *    @x：待插入排序的整数
+ *    返回值：
+ *         无返回值
+ */
+void Insert(int a[],int n,int x )
+{
+    int l = 0;
+    int r = n -2;
+    int mid = 0;
+    while (l <= r)
+    {
+       mid = (l + r) / 2;
+       if(x > a[mid])
+       {
+            l = mid + 1;
+       }
+       else if( x < a[mid])
+       {
+            r = mid - 1;
+       }
+    }
+    int k = 0;
+    for(k = n - 2; k >= l; k--)
+    {
+        a[k+1] = a[k];
+    }
+    a[l] = x;
+}
 int main()
 {
     int a[10] = {2,3,1,5,9,6,4,8,0,10};
     int i = 0;
-    int j = 0;
-    int n = sizeof(a) /sizeof(a[0]);
-    int k = 0;
-    for(i = 1 ; i < n-1;i++)
+    int n = sizeof(a)/ sizeof(a[0]);
+    for(i = 1; i < n ;i++)
     {
-        for(j = 0 ; j < i ;j++)
-        {
-            if(a[j] > a[i])
-            {
-                break;
-            }
-        }
-        int x = a[i];
-        for (k = i - 1; k >= j; k--)
-        {
-            a[k+1] = a[k];
-        }
-        a[j] = x;
+       Insert(a,i+1,a[i]);
     }
-    for(i = 0 ; i < n ;i++)
+    for( i = 0 ; i < n ;i++)
     {
         printf("%d ",a[i]);
     }
