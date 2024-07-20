beafa967726d024cd0411af49a4d3d61016ab689
wangjun <1556405648@qq.com>
2024-07-20 15:50:29 +0800

    插入排序-遍历的方式排序

--- test_7_20.c
+++ test_7_20.c
@@ -15,33 +15,67 @@
  *选择排序
  *
  */
+// int main()
+// {
+//     int a[10] = {2,3,1,5,9,6,4,8,0,10};
+//     int i = 0;
+//     int n = sizeof(a) /sizeof(a[0]);
+//     int j = 0;
+//     for(i = 0;i < n -1;i++)
+//     {
+//         int max = INT32_MIN;
+//         int max_index = i;
+//         for(j = 0; j < n-i;j++)
+//         {
+//             if(a[j] > max)
+//             {
+//                 max = a[j];
+//                 max_index = j;
+//
+//             }
+//         }
+//         if(max_index != n - i - 1)
+//         {
+//             int temp = a[max_index];
+//             a[max_index] = a[n -i -1];
+//             a[n - i -1] = temp;
+//         }
+//     }
+//     for(i = 0; i < n  ;i++)
+//     {
+//         printf("%d ",a[i]);
+//     }
+//     return 0;
+// }
+
+/**
+ *
+ * 插入排序-遍历的方式排序
+ */
 int main()
 {
     int a[10] = {2,3,1,5,9,6,4,8,0,10};
     int i = 0;
-    int n = sizeof(a) /sizeof(a[0]);
     int j = 0;
-    for(i = 0;i < n -1;i++)
+    int n = sizeof(a) /sizeof(a[0]);
+    int k = 0;
+    for(i = 1 ; i < n-1;i++)
     {
-        int max = INT32_MIN;
-        int max_index = i;
-        for(j = 0; j < n-i;j++)
+        for(j = 0 ; j < i ;j++)
         {
-            if(a[j] > max)
+            if(a[j] > a[i])
             {
-                max = a[j];
-                max_index = j;
-
+                break;
             }
         }
-        if(max_index != n - i - 1)
+        int x = a[i];
+        for (k = i - 1; k >= j; k--)
         {
-            int temp = a[max_index];
-            a[max_index] = a[n -i -1];
-            a[n - i -1] = temp;
+            a[k+1] = a[k];
         }
+        a[j] = x;
     }
-    for(i = 0; i < n  ;i++)
+    for(i = 0 ; i < n ;i++)
     {
         printf("%d ",a[i]);
     }
