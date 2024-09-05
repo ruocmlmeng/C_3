90fff42a6aa43fbbff78c316be4c4ee68709dba2
wangjun <1556405648@qq.com>
2024-09-05 21:25:37 +0800

    111

--- test_7_9.c
+++ test_7_9.c
@@ -852,22 +852,41 @@
  *  返回值：
  *      返回一维数组的元素之和 int
  */
+// int sum_array(int a[],int n)
+// {
+//     int sum = 0;
+//     int i = 0;
+//     for (i = 0 ;i < n ;i++)
+//     {
+//       sum += a[i];
+//     }
+//     return sum;
+// }
+// int main()
+// {
+//     int a[10] = {1,2,3,4,5,6,7,8,9,10};
+//     int  (*p)(int * ,int);
+//     p = sum_array;
+//     int sum = p(a,10);
+//     printf("sum == %d\n",sum);
+//     return 0;
+// }
 int sum_array(int a[],int n)
 {
-    int sum = 0;
-    int i = 0;
-    for (i = 0 ;i < n ;i++)
-    {
-      sum += a[i];
-    }
-    return sum;
+ int sum = 0;
+ int i = 0;
+ for (i = 0 ;i < n ;i++)
+ {
+  sum += a[i];
+ }
+ return sum;
 }
 int main()
 {
-    int a[10] = {1,2,3,4,5,6,7,8,9,10};
-    int  (*p)(int * ,int);
-    p = sum_array;
-    int sum = p(a,10);
-    printf("sum == %d\n",sum);
-    return 0;
+ int a[10] = {1,2,3,4,5,6,7,8,9,10};
+ int  (*p)(int * ,int);
+ p = sum_array;
+ int sum = p(a,10);
+ printf("sum == %d\n",sum);
+ return 0;
 }
